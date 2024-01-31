cb52298b4f207049d64735aa275b3328c6f7abe4
Kreeevin <digimonlord2@gmail.com>
2024-01-31 11:27:04 -0500

    Fixed case sensitivity issues on Linux

--- teamClyde/project1/variant1.py
+++ teamClyde/project1/variant1.py
@@ -1,13 +1,13 @@
 # This is necessary to find the main code
 import sys
-sys.path.insert(0, '../../bomberman')
+sys.path.insert(0, '../../Bomberman')
 sys.path.insert(1, '..')
 
 # Import necessary stuff
 from game import Game
 
 # TODO This is your code!
-sys.path.insert(1, '../teamNN')
+sys.path.insert(1, '../teamClyde')
 
 # Uncomment this if you want the empty test character
 #from testcharacter import TestCharacter
@@ -28,7 +28,7 @@ g = Game.fromfile('map.txt')
 
 # Uncomment this if you want the interactive character
 g.add_character(InteractiveCharacter("me", # name
-                                     "C",  # avatar
+                                     "🚶",  # avatar
                                      0, 0  # position
 ))
 
